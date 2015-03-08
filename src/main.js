6bf9ebbb9d42b638eed3de84c1ab8b821dd2a1af
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-03-08 13:18:08 +0000

    Got starfield movement hooked up and in sync with slides (except for when you reach end of the slides or change the hash - need to fix those things still)

--- src/main.js
+++ src/main.js
@@ -4,3 +4,55 @@ var starfield = require('./starfield'),
 impress().init();
 
 starfield.init();
+
+var slideElements = document.getElementById('impress').querySelectorAll('.slide');
+
+/*
+document.addEventListener('impress:stepleave', function(e) {
+
+    console.log('step leave', e);
+
+    var currentSlide = e.target;
+
+    console.log('child nodes', slideElements);
+    console.log('current slide', currentSlide);
+
+    var currentSlideNumber = Array.prototype.indexOf.call(slideElements, currentSlide);
+
+    console.log('current slide number', currentSlideNumber);
+
+});
+*/
+
+function onPrevSlide() {
+    starfield.moveCameraZ(10);
+}
+
+function onNextSlide() {
+    starfield.moveCameraZ(-10);
+}
+
+/**
+ * TODO also handle hash change!
+ */
+document.addEventListener("keyup", function ( event ) {
+    if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+        switch( event.keyCode ) {
+            case 33: // pg up
+            case 37: // left
+            case 38: // up
+                onPrevSlide();
+                break;
+            case 9:  // tab
+            case 32: // space
+            case 34: // pg down
+            case 39: // right
+            case 40: // down
+                onNextSlide();
+                break;
+        }
+
+        event.preventDefault();
+    }
+}, false);
+
--- src/starfield.js
+++ src/starfield.js
@@ -1,9 +1,10 @@
 /**
- * Based on Seb Lee Delisle's Three.js tutorial - see LICENCE.txt.
+ * Originally based on Seb Lee Delisle's Three.js tutorial - see LICENCE.txt.
  */
 
 require('../vendor/threejs-extras/Projector.js');
 require('../vendor/threejs-extras/CanvasRenderer.js');
+var TWEEN = require('tween.js');
 
 console.log('THREE', THREE);
 
@@ -13,12 +14,14 @@ var Starfield = function() {
         scene,
         renderer,
         particles,
+        starTexture = THREE.ImageUtils.loadTexture('../images/star.png'),
         width = window.innerWidth,
         height = window.innerHeight;
 
+
     this.init = function() {
 
-        camera = new THREE.PerspectiveCamera(75, width / height, 1, 3000 );
+        camera = new THREE.PerspectiveCamera(75, width / height, 1, 5000 );
         camera.position.z = 1000;
 
         scene = new THREE.Scene();
@@ -35,11 +38,21 @@ var Starfield = function() {
 
     };
 
-    function animate() {
+    this.moveCameraZ = function(amount) {
 
-        updateParticles();
+        new TWEEN.Tween( camera.position )
+            .to( { z: camera.position.z + amount }, 1000 )
+            .easing( TWEEN.Easing.Quadratic.InOut )
+            .start();
 
-        // and render the scene from the perspective of the camera
+//        camera.position.z -= amount;
+
+    };
+
+    function animate(time) {
+
+        TWEEN.update(time);
+
         renderer.render( scene, camera );
 
         requestAnimationFrame( animate );
@@ -54,32 +67,25 @@ var Starfield = function() {
         var material,
             geometry = new THREE.Geometry();
 
-        for( var i = 0; i < 20000; i ++ ) {
+        for( var i = 0; i < 50000; i ++ ) {
 
             var vertex = new THREE.Vector3();
             vertex.x = Math.random() * 2000 - 1000;
             vertex.y = Math.random() * 2000 - 1000;
-            vertex.z = Math.random() * -2000 + 1000;
+            vertex.z = Math.random() * 2000 - 1000;
 
             geometry.vertices.push( vertex );
 
         }
 
-        // we make a particle material and pass through the
-        // colour and custom particle render function we defined.
-        material = new THREE.PointCloudMaterial( { color: 0xffffff } );
+        material = new THREE.PointCloudMaterial( { size: 5, color: 0xffffff, map: starTexture, transparent: true } );
 
-        // make the particle
+        //material.map.repeat.set( 1, 1, 1 );
+        material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+
         particles = new THREE.PointCloud( geometry, material );
 
-        // add it to the scene
         scene.add( particles );
-
-    }
-
-    function updateParticles() {
-
-        // TODO
 
     }
 
